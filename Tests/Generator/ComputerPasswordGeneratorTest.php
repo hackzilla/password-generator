bbf6804a65c9f89f05a8ae6bf93700a52eff8d69
Daniel Platt <github@ofdan.co.uk>
2014-04-21 21:58:09 +0100

    rename test to reflect renamed class

--- Tests/Generator/ComputerPasswordGeneratorTest.php
+++ Tests/Generator/ComputerPasswordGeneratorTest.php
@@ -2,16 +2,16 @@
 
 namespace Hackzilla\PasswordGenerator\Tests\Generator;
 
-use Hackzilla\PasswordGenerator\Generator\PasswordGenerator;
+use Hackzilla\PasswordGenerator\Generator\ComputerPasswordGenerator;
 
-class PasswordGeneratorTest extends \PHPUnit_Framework_TestCase
+class ComputerPasswordGeneratorTest extends \PHPUnit_Framework_TestCase
 {
 
     private $_object;
 
     public function setup()
     {
-        $this->_object = new PasswordGenerator();
+        $this->_object = new ComputerPasswordGenerator();
     }
 
     /**
@@ -67,11 +67,22 @@ class PasswordGeneratorTest extends \PHPUnit_Framework_TestCase
     public function optionProvider()
     {
         return array(
-            array(PasswordGenerator::OPTION_UPPER_CASE, array('A', 'B', 'C'), array('a', 'b', 'c')),
-            array(PasswordGenerator::OPTION_LOWER_CASE, array('a', 'b', 'c'), array('A', 'B', 'C')),
-            array(PasswordGenerator::OPTION_NUMBERS, array('1', '2', '3'), array('a', 'b', 'c', 'A', 'B', 'C')),
-            array(PasswordGenerator::OPTION_SYMBOLS, array('+', '=', '?'), array('a', 'b', 'c', 'A', 'B', 'C')),
-            array(PasswordGenerator::OPTION_LOWER_CASE | PasswordGenerator::OPTION_UPPER_CASE | PasswordGenerator::OPTION_AVOID_SIMILAR, array('a', 'b', 'c', 'A', 'B', 'C'), array('o', 'l', 'O')),
+            array(ComputerPasswordGenerator::OPTION_UPPER_CASE, array('A', 'B', 'C'), array('a', 'b', 'c')),
+            array(ComputerPasswordGenerator::OPTION_LOWER_CASE, array('a', 'b', 'c'), array('A', 'B', 'C')),
+            array(ComputerPasswordGenerator::OPTION_NUMBERS, array('1', '2', '3'), array('a', 'b', 'c', 'A', 'B', 'C')),
+            array(ComputerPasswordGenerator::OPTION_SYMBOLS, array('+', '=', '?'), array('a', 'b', 'c', 'A', 'B', 'C')),
+            array(ComputerPasswordGenerator::OPTION_LOWER_CASE | ComputerPasswordGenerator::OPTION_UPPER_CASE | ComputerPasswordGenerator::OPTION_AVOID_SIMILAR, array('a', 'b', 'c', 'A', 'B', 'C'), array('o', 'l', 'O')),
+        );
+    }
+
+    public function optionsProvider()
+    {
+        return array(
+            ComputerPasswordGenerator::OPTION_UPPER_CASE,
+            ComputerPasswordGenerator::OPTION_LOWER_CASE,
+            ComputerPasswordGenerator::OPTION_NUMBERS,
+            ComputerPasswordGenerator::OPTION_SYMBOLS,
+            ComputerPasswordGenerator::OPTION_AVOID_SIMILAR,
         );
     }
 
