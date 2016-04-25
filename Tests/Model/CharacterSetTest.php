d13c2236cf90044694b436688a73806193063d36
Daniel Platt <github@ofdan.co.uk>
2016-04-25 10:57:45 +0100

    split test for PR #15

--- Tests/Model/CharacterSetTest.php
+++ Tests/Model/CharacterSetTest.php
@@ -17,7 +17,6 @@ class CharacterSetTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($result, $characterSet->getCharacters());
         $this->assertEquals($result, $characterSet->__toString());
-        $this->assertEquals($result, (string) $characterSet);
     }
 
     public function characterProvider()
@@ -26,6 +25,27 @@ class CharacterSetTest extends \PHPUnit_Framework_TestCase
             array('ABC', 'ABC'),
             array('', ''),
             array(null, ''),
+        );
+    }
+
+    /**
+     * @dataProvider castCharacterProvider
+     * @param $characters
+     * @param $result
+     */
+    public function testConstructCast($characters, $result)
+    {
+        $characterSet = new CharacterSet($characters);
+
+        $this->assertEquals($result, (string) $characterSet);
+    }
+
+    public function castCharacterProvider()
+    {
+        return array(
+            array('ABC', 'ABC'),
+            array('', ''),
+            array(null, null),
         );
     }
 }
